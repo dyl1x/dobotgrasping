74bc489e5e2e75cc6f541cd6395b66da956fdbff
Finn2019 <finn.linstrom9@gmail.com>
2022-04-25 21:05:20 +1000

    created func for sift,surf features and dag network

---
+++ working/test_network.m
@@ -0,0 +1,13 @@
+function test_network(net, image)
+
+I = imread(image);
+R = imresize(I, [224, 224]); % image size
+
+[label, probability] = classify(net, R);
+disp(label)
+disp(probability)
+figure;
+imshow(R);
+title(strcat([char(label), ': ', num2str(max(probability)*100, 5)]));
+
+end
