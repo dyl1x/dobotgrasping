62c421e863a25b1356ec0e7ed45b2bc981fa7dc7
Chamath E <chamathme@gmail.com>
2022-05-05 20:59:23 +1000

    real robot

---
+++ working/real_rob_snc.m
@@ -0,0 +1,153 @@
+rosinit;
+
+safetyStatusSubscriber = rossubscriber('/dobot_magician/safety_status');
+pause(2); %Allow some time for MATLAB to start the subscriber
+currentSafetyStatus = safetyStatusSubscriber.LatestMessage.Data
+
+while (currentSafetyStatus == 4) %re initialize
+    disp('reinit...');
+    [safetyStatePublisher,safetyStateMsg] = rospublisher('/dobot_magician/target_safety_status');
+    safetyStateMsg.Data = 2;
+    send(safetyStatePublisher,safetyStateMsg);
+    pause(5);
+    
+    disp('checking...');
+    currentSafetyStatus = safetyStatusSubscriber.LatestMessage.Data
+    pause(0.1);
+end
+
+disp('proceding...');
+
+%% Get current tool state
+
+toolStateSubscriber = rossubscriber('/dobot_magician/tool_state');
+pause(2); %Allow some time for MATLAB to start the subscriber
+currentToolState = toolStateSubscriber.LatestMessage.Data;
+
+if currentToolState == 1   
+    % Turn off the tool
+    [toolStatePub, toolStateMsg] = rospublisher('/dobot_magician/target_tool_state');
+    toolStateMsg.Data = [0]; % Send 1 for on and 0 for off
+    send(toolStatePub,toolStateMsg);   
+end
+%% ESTOP
+
+%run this section to stop
+
+[safetyStatePublisher,safetyStateMsg] = rospublisher('/dobot_magician/target_safety_status');
+safetyStateMsg.Data = 3;
+send(safetyStatePublisher,safetyStateMsg);
+
+%%
+
+dropPoints = [1,2,1;
+              3,4,1;
+              5,6,1];
+          
+pickPoints = [1,2,1;
+              3,4,1;
+              5,6,1];
+
+endEffectorPoseSubscriber = rossubscriber('/dobot_magician/end_effector_poses'); % Create a ROS Subscriber to the topic end_effector_poses
+pause(2); %Allow some time for MATLAB to start the subscriber
+currentEndEffectorPoseMsg = endEffectorPoseSubscriber.LatestMessage;
+% Extract the position of the end effector from the received message
+currentEndEffectorPosition = [currentEndEffectorPoseMsg.Pose.Position.X,
+                              currentEndEffectorPoseMsg.Pose.Position.Y,
+                              currentEndEffectorPoseMsg.Pose.Position.Z]
+% Extract the orientation of the end effector
+currentEndEffectorQuat = [currentEndEffectorPoseMsg.Pose.Orientation.W,
+                          currentEndEffectorPoseMsg.Pose.Orientation.X,
+                          currentEndEffectorPoseMsg.Pose.Orientation.Y,
+                          currentEndEffectorPoseMsg.Pose.Orientation.Z];
+% Convert from quaternion to euler
+%[roll,pitch,yaw] = quat2eul(currentEndEffectorQuat);
+
+eul = quat2eul(currentEndEffectorQuat');     
+
+% move to pick point 1
+% this is the original end effector pose of the Dobot without any tool attached
+endEffectorPosition = [0,0,0]; % home
+endEffectorRotation = [0,0,0]; % home
+
+[targetEndEffectorPub,targetEndEffectorMsg] = rospublisher('/dobot_magician/target_end_effector_pose');
+
+targetEndEffectorMsg.Position.X = endEffectorPosition(1);
+targetEndEffectorMsg.Position.Y = endEffectorPosition(2);
+targetEndEffectorMsg.Position.Z = endEffectorPosition(3);
+
+qua = eul2quat(endEffectorRotation);
+targetEndEffectorMsg.Orientation.W = qua(1);
+targetEndEffectorMsg.Orientation.X = qua(2);
+targetEndEffectorMsg.Orientation.Y = qua(3);
+targetEndEffectorMsg.Orientation.Z = qua(4);
+
+send(targetEndEffectorPub,targetEndEffectorMsg);
+
+%% activate tool
+%[toolStatePub, toolStateMsg] = rospublisher('/dobot_magician/target_tool_state');
+toolStateMsg.Data = [1]; % Send 1 for on and 0 for off 
+send(toolStatePub,toolStateMsg);
+
+%% move up
+
+endEffectorPosition = [0,0,0]; % home
+endEffectorRotation = [0,0,0]; % home
+
+%[targetEndEffectorPub,targetEndEffectorMsg] = rospublisher('/dobot_magician/target_end_effector_pose');
+
+targetEndEffectorMsg.Position.X = endEffectorPosition(1);
+targetEndEffectorMsg.Position.Y = endEffectorPosition(2);
+targetEndEffectorMsg.Position.Z = endEffectorPosition(3);
+
+qua = eul2quat(endEffectorRotation);
+targetEndEffectorMsg.Orientation.W = qua(1);
+targetEndEffectorMsg.Orientation.X = qua(2);
+targetEndEffectorMsg.Orientation.Y = qua(3);
+targetEndEffectorMsg.Orientation.Z = qua(4);
+
+send(targetEndEffectorPub,targetEndEffectorMsg);
+
+%% move to drop
+
+endEffectorPosition = [0,0,0]; % home
+endEffectorRotation = [0,0,0]; % home
+
+%[targetEndEffectorPub,targetEndEffectorMsg] = rospublisher('/dobot_magician/target_end_effector_pose');
+
+targetEndEffectorMsg.Position.X = endEffectorPosition(1);
+targetEndEffectorMsg.Position.Y = endEffectorPosition(2);
+targetEndEffectorMsg.Position.Z = endEffectorPosition(3);
+
+qua = eul2quat(endEffectorRotation);
+targetEndEffectorMsg.Orientation.W = qua(1);
+targetEndEffectorMsg.Orientation.X = qua(2);
+targetEndEffectorMsg.Orientation.Y = qua(3);
+targetEndEffectorMsg.Orientation.Z = qua(4);
+
+send(targetEndEffectorPub,targetEndEffectorMsg);
+
+%% drop
+
+%[toolStatePub, toolStateMsg] = rospublisher('/dobot_magician/target_tool_state');
+toolStateMsg.Data = [0]; % Send 1 for on and 0 for off 
+send(toolStatePub,toolStateMsg);
+
+%% move to home
+
+endEffectorPosition = [0,0,0]; % home
+endEffectorRotation = [0,0,0]; % home
+
+%[targetEndEffectorPub,targetEndEffectorMsg] = rospublisher('/dobot_magician/target_end_effector_pose');
+
+targetEndEffectorMsg.Position.X = endEffectorPosition(1);
+targetEndEffectorMsg.Position.Y = endEffectorPosition(2);
+targetEndEffectorMsg.Position.Z = endEffectorPosition(3);
+
+qua = eul2quat(endEffectorRotation);
+targetEndEffectorMsg.Orientation.W = qua(1);
+targetEndEffectorMsg.Orientation.X = qua(2);
+targetEndEffectorMsg.Orientation.Y = qua(3);
+targetEndEffectorMsg.Orientation.Z = qua(4);
+
+send(targetEndEffectorPub,targetEndEffectorMsg);
