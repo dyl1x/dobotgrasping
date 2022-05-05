37b80f933e57cd4cf0664e49ba545ca7cb6d4943
Chamath E <chamathme@gmail.com>
2022-05-06 09:32:45 +1000

    improvements

--- working/artag/localize.m
+++ working/artag/localize.m
@@ -1,5 +1,5 @@
-img4 = imread("ss2.png");
-img4 = rgb2gray(img4);
+img4 = imread("tag.jpg");
+%img4 = rgb2gray(img4);
 cp = detectHarrisFeatures(img4);
 
 leftTop = cp.Location(1,:);
@@ -16,7 +16,7 @@ plot(cp.Location(:,1),cp.Location(:,2),'*g');
 
 pause(0.01)
 
-sample = imread("ss.png");
+sample = imread("img.jpg");
 sample = rgb2gray(sample);
 cp2 = detectHarrisFeatures(sample);
 
@@ -30,8 +30,8 @@ plot(cp2.Location(:,1),cp2.Location(:,2),'*g');
 i1gs = img4;
 i2gs = sample;
 % feature decriptors, corresponding location
-[features1, validPoints1] = extractFeatures(i1gs, cp, 'Method' , 'Block', 'BlockSize',7);
-[features2, validPoints2] = extractFeatures(i2gs, cp2, 'Method' , 'Block', 'BlockSize',7);
+[features1, validPoints1] = extractFeatures(i1gs, cp, 'Method' , 'Block', 'BlockSize',5);
+[features2, validPoints2] = extractFeatures(i2gs, cp2, 'Method' , 'Block', 'BlockSize',5);
 
 % match features
 indexPairs = matchFeatures(features1,features2);
@@ -42,12 +42,4 @@ matchedPoints2 = validPoints2(indexPairs(:,2));
 figure;
 showMatchedFeatures(i1gs,i2gs,matchedPoints1,matchedPoints2,'montage')
 
-%%
 
-[id,loc] = readAprilTag(img4);
-
-cp2.Location
-figure;
-imshow(img4);
-hold on
-plot(loc(:,1),loc(:,2),'*g');
