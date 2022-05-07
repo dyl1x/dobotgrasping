9009605157131fa60c3876e25f853e52ae33066d
Finn2019 <finn.linstrom9@gmail.com>
2022-05-07 21:30:24 +1000

    began rmrc path tracing

--- working/main_IR.m
+++ working/main_IR.m
@@ -41,7 +41,7 @@ hold off
 
 % add pcbs
 hold on
-pcb1 = PCB(1,transl(-0.375,0,0.5));
+pcb1 = PCB(1,transl(-0.375,-0.1,0.5));
 pcb2 = PCB(2,transl(-0.375,0.3,0.5));
 pcb3 = PCB(3,transl(-0.375,0.5,0.5));
 
@@ -52,13 +52,17 @@ qdrop = [1.5708, 0.0017, -0.1257, 0];
 
 
 % pick and place pcb 1
+current_pose = r2.model.fkine(r2.model.getpos);
 q1 = r2.model.ikcon(pcb1.pose, q0);
-qmatrix = calc_trap_qmatrix(q0, q1, 10);
+[qmatrix, desired] = rmrc(current_pose(1:3, 4)', pcb1.pose(1:3, 4)', q0, r2.model, false, false);
+% qmatrix = calc_trap_qmatrix(q0, q1, 10);
+
+plot3(desired(1, :), desired(2, :), desired(3, :), 'm.', 'LineWidth', 1);
+
 for i=1:size(qmatrix)
    r2.model.animate(qmatrix(i,:)); 
-   pause(0.01);
+   pause(0.03);
 end
-
 
 %% lift up
 r2qCurrent = r2.model.getpos;
