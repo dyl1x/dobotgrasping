f411bd7fd749479663c7f6002d99ec62b7cde8cb
Finn2019 <finn.linstrom9@gmail.com>
2022-05-18 08:09:01 +1000

    final

--- working/LAB2GUI.m
+++ working/LAB2GUI.m
@@ -43,7 +43,7 @@ end
 
 % --- Executes just before LAB2GUI is made visible.
 function LAB2GUI_OpeningFcn(hObject, eventdata, handles, varargin)
-
+cla
 handles.exitbutton.UserData = 0;
 handles.simstarter.UserData = 0;
 
@@ -119,7 +119,7 @@ axis equal
 
 pcbs = [];
 pcbs{1} = PCB(1, transl(-0.33, 0, handles.h) * trotz(pi/2));
-pcbs{2} = PCB(2, transl(-0.31, 0.3, handles.h) * trotz(pi));
+pcbs{2} = PCB(2, transl(-0.34, 0.3, handles.h) * trotz(pi));
 pcbs{3} = PCB(3, transl(-0.33, 0.6, handles.h));
 
 view([0 0 1]);
@@ -189,20 +189,20 @@ function [handles] = animate_traj(handles, dest, model, obj, path, weight, plot,
                         handles.current_traces{j}.ZData = xyz_(3);
 
                     end
-%                 elseif i == length(qmatrix)
-%                     obj_pos = ee(1:3, 4);
-%                     for j=1:length(handles.current_traces)
-% 
-%                         x_ = handles.current_traces{j}.XData;
-%                         y_ = handles.current_traces{j}.YData;
-%                         z_ = handles.current_traces{j}.ZData;
-% 
-%                         xyz_ = inv(init_rot) * [x_ - obj_pos(1), y_ - obj_pos(2), z_]';
-% 
-%                         handles.current_traces{j}.XData = obj_pos(1) - xyz_(1);
-%                         handles.current_traces{j}.YData = obj_pos(2) - xyz_(2);
-%                         handles.current_traces{j}.ZData = xyz_(3);
-%                     end
+                elseif i == length(qmatrix)
+                    obj_pos = ee(1:3, 4);
+                    for j=1:length(handles.current_traces)
+
+                        x_ = handles.current_traces{j}.XData;
+                        y_ = handles.current_traces{j}.YData;
+                        z_ = handles.current_traces{j}.ZData;
+
+                        xyz_ = inv(init_rot) * [x_ - obj_pos(1), y_ - obj_pos(2), z_]';
+
+                        handles.current_traces{j}.XData = obj_pos(1) - xyz_(1);
+                        handles.current_traces{j}.YData = obj_pos(2) - xyz_(2);
+                        handles.current_traces{j}.ZData = xyz_(3);
+                    end
                 else
                     obj_pos = ee(1:3, 4);
                     prev_pos = prev_ee(1:3, 4);
@@ -327,14 +327,13 @@ function [qm1, qm2] = collision_detection(qm1, m1, qm2, m2, plot)
                  ; cubePoints * rotz(3*pi/2) ...
                  ; cubePoints * roty(pi/2) ...
                  ; cubePoints * roty(-pi/2)];         
-         
-    % Plot the cube's point cloud         
-%     cubeAtOrigin_h = plot3(cubePoints(:,1), cubePoints(:,2), cubePoints(:,3), 'r.');
-
-
+    
     for s = 1 : length(qm1)
         q1 = qm1(s, :);
+        m1.animate(q1);
+
         q2 = qm2(s, :);
+        m2.animate(q2);
         
         tr1 = zeros(4, 4, m1.n + 1);
         tr2 = zeros(4, 4, m2.n + 1);
@@ -347,6 +346,8 @@ function [qm1, qm2] = collision_detection(qm1, m1, qm2, m2, plot)
 
         for i = 1 : m1.n
             tr1(:, :, i+1) = tr1(:, :, i) * trotz(q1(i)) * transl(0,0,L1(i).d) * transl(L1(i).a,0,0) * trotx(L1(i).alpha);
+        end
+        for i = 1 : m2.n
             tr2(:, :, i+1) = tr2(:, :, i) * trotz(q2(i)) * transl(0,0,L2(i).d) * transl(L2(i).a,0,0) * trotx(L2(i).alpha);
         end
 
@@ -356,8 +357,10 @@ function [qm1, qm2] = collision_detection(qm1, m1, qm2, m2, plot)
             updatedCubePoints = cubePointsAndOnes(:, 1:3);
             algebraicDist = GetAlgebraicDist(updatedCubePoints, tr1(1:3, 4, i)', radii);
             pointsInside = find(algebraicDist < 1);
-%             disp(['There are ', num2str(size(pointsInside,1)),' points inside the ', num2str(i),'th ellipsoid']);
-        end
+            if size(pointsInside, 1) ~= 0
+                disp(['There are ', num2str(size(pointsInside,1)),' points inside the ', num2str(i),'th ellipsoid']);
+            end
+        end     
 
         % cubes from m1 in links of m2
         for i = 1 : size(tr2, 3)
@@ -365,7 +368,9 @@ function [qm1, qm2] = collision_detection(qm1, m1, qm2, m2, plot)
             updatedCubePoints = cubePointsAndOnes(:, 1:3);
             algebraicDist = GetAlgebraicDist(updatedCubePoints, tr2(1:3, 4, i)', radii);
             pointsInside = find(algebraicDist < 1);
-%             disp(['There are ', num2str(size(pointsInside,1)),' points inside the ', num2str(i),'th ellipsoid']);
+            if size(pointsInside, 1) ~= 0
+                disp(['There are ', num2str(size(pointsInside,1)),' points inside the ', num2str(i),'th ellipsoid']);
+            end
         end
     end
         
@@ -555,11 +560,13 @@ if isfield(handles,'r1')
         
         steps = 9;
         
-        if l > 3, break; end
         if handles.exitbutton.UserData == 1, break; end
-        
+        if l > 4, break; end
+
         if mod(i, steps * l) == 0
             l = l + 1;
+        elseif l == 4 && mod(i, steps) == 1
+            break;
         else
             handles = sequences(i, steps, handles);
             guidata(hObject, handles);
@@ -567,15 +574,15 @@ if isfield(handles,'r1')
         end
     end
     
+    disp('Completed Trace Paths')
     if handles.exitbutton.UserData == 1
         disp('Returning out of sim start')
         closereq();
         %     return;
     end
 else
-    errordlg('Please load the map and assets first','Asset error');
+    errordlg('Please load the map and assets first', 'Asset error');
 end
-
 
 
 function [handles] = sequences(i, steps, handles)
@@ -811,6 +818,7 @@ handles.radiobutton2.Value = 0;
 handles.robot = handles.r1;
 update_strings(hObject,handles)
 disp('Teach Panel switched to Robot 1')
+plot
 
 
 % --- Executes on button press in radiobutton2.
@@ -819,6 +827,35 @@ handles.radiobutton1.Value = 0;
 handles.robot = handles.r2;
 update_strings(hObject,handles)
 disp('Teach Panel switched to Robot 2')
+
+
+function forcedCol_Callback(hObject, eventdata, handles)
+if handles.simstarter.UserData == 1
+    disp('Please stop the simulation first');
+else
+
+    disp('Moving into Scenarios where both robots move to avoid a collision')
+    handles.r1.model.animate([0 pi/4 pi/2 pi/4 0])
+    handles.r2.model.animate([0 pi/4 pi/2 pi/4 0])
+
+    disp('Scenario 1: Transporter intercept laser')
+    handles.r1.model.animate([pi/8 pi/4 pi/2 pi/4 0]) % start
+    handles.r2.model.animate([-pi/2 pi/4 pi/2 pi/4 0]) % start
+
+    dest1 = transl(0, -0.5, 0.5);
+    [qmatrix1, desired1] = rmrc(handles.r1.model.fkine(handles.r1.model.getpos), dest1, handles.r1.model.getpos, handles.r1.model, false, 1, false); 
+    plt1 = plot3(desired1(1, :), desired1(2, :), desired1(3, :), 'm*');
+
+    dest2 = transl(-0.12, -0.5, 0.5);
+    [qmatrix2, desired2] = rmrc(handles.r2.model.fkine(handles.r2.model.getpos), dest2, handles.r2.model.getpos, handles.r2.model, false, 1, false); 
+    plt2 = plot3(desired2(1, :), desired2(2, :), desired2(3, :), 'g*');
+
+    [qm1, qm2] = collision_detection(qmatrix1, handles.r1.model, qmatrix2, handles.r2.model, true);
+    
+    plt2 = plot3(desired2(1, :), desired2(2, :), desired2(3, :), 'c*');
+    disp('')
+end
+
 
 
 % Q's
